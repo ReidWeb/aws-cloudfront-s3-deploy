16504266b666610fa367f9e6d8a2ec9d0ee674f8
Peter Reid <peter@reidweb.com>
2018-09-13 17:48:30 +0100

    test(s3): Add more tests for s3.js and fix linting errors

--- test/testS3.js
+++ test/testS3.js
@@ -1,58 +1,56 @@
-'use strict';
+/* eslint-disable no-underscore-dangle */
 /* eslint-env mocha */
 
 const chai = require('chai');
 const rewire = require('rewire');
+
 const should = chai.should();
-const expect = chai.expect;
 const sinon = require('sinon');
-chai.use(require('chai-as-promised'))
+chai.use(require('chai-as-promised'));
 
-describe("s3.js [Unit]", ()=> {
-
+describe('s3.js [Unit]', () => {
   describe('#getFileLastModifiedDate', () => {
-
-    it('Should resolve the last modified date when the meta-data is available', async () => {
+    it('should resolve the last modified date when the meta-data is available', async () => {
       const s3 = rewire('../src/s3');
       const getFileLastModifiedDate = s3.__get__('getFileLastModifiedDate');
-      const time = "2014-09-12T15:48:06.228Z";
+      const time = '2014-09-12T15:48:06.228Z';
       const fsMock = {
-        stat: function (path, cb) {
-          cb(null, {"mtime" : time});
-        }
+        stat(path, cb) {
+          cb(null, { mtime: time });
+        },
       };
 
-      s3.__set__({fs:fsMock});
+      s3.__set__({ fs: fsMock });
 
       const lastModified = await getFileLastModifiedDate('package.json');
       lastModified.should.equal(time);
     });
 
-    it('Should resolve to `false` when the meta-data is non existant', async () => {
+    it('should resolve to `false` when the meta-data is non existant', async () => {
       const s3 = rewire('../src/s3');
       const getFileLastModifiedDate = s3.__get__('getFileLastModifiedDate');
       const fsMock = {
-        stat: function (path, cb) {
+        stat(path, cb) {
           cb(null, {});
-        }
+        },
       };
 
-      s3.__set__({fs:fsMock});
+      s3.__set__({ fs: fsMock });
 
       const actual = await getFileLastModifiedDate('package.json');
       actual.should.equal(false);
     });
 
-    it('Should resolve to `false` when the meta-data is an empty string', async () => {
+    it('should resolve to `false` when the meta-data is an empty string', async () => {
       const s3 = rewire('../src/s3');
       const getFileLastModifiedDate = s3.__get__('getFileLastModifiedDate');
       const fsMock = {
-        stat: function (path, cb) {
-          cb(null, {"mtime" : ""});
-        }
+        stat(path, cb) {
+          cb(null, { mtime: '' });
+        },
       };
 
-      s3.__set__({fs:fsMock});
+      s3.__set__({ fs: fsMock });
 
       const actual = await getFileLastModifiedDate('package.json');
       actual.should.equal(false);
@@ -60,98 +58,184 @@ describe("s3.js [Unit]", ()=> {
   });
 
   describe('#uploadObj', () => {
-
-    it('Should resolve the object key upon a successful upload', async () => {
+    it('should resolve the object key upon a successful upload', async () => {
       const s3 = rewire('../src/s3');
       const uploadObj = s3.__get__('uploadObj');
 
       const s3ClientMock = {
-        putObject: function (params, cb) {
-          cb(null,{})
-        }
+        putObject(params, cb) {
+          cb(null, {});
+        },
       };
 
-      let actual = await uploadObj({"Key": "hiThere"}, s3ClientMock, 0);
+      const actual = await uploadObj({ Key: 'hiThere' }, s3ClientMock, 0);
 
-      actual.should.equal("hiThere");
+      actual.should.equal('hiThere');
     });
 
-    it('Should reject with an error after multiple failed uploads',  async () => {
+    it('should reject with an error after multiple failed uploads', async () => {
       const s3 = rewire('../src/s3');
       const uploadObj = s3.__get__('uploadObj');
 
       const s3ClientMock = {
-        putObject: function(params, cb) {
+        putObject(params, cb) {
           cb(new Error('Generic error'), {});
-        }
+        },
       };
 
-      try{
-        let x = await uploadObj({"Key": "foo"}, s3ClientMock, 4)
-        "I".should.not.equal("Me");
+      try {
+        await uploadObj({ Key: 'foo' }, s3ClientMock, 4);
+        'I'.should.equal('Me');
       } catch (e) {
-        e.message.should.equal("Error: Unable to process object foo, reattempted for 5 (MAX)");
+        e.message.should.equal('Error: Unable to process object foo, reattempted for 5 (MAX)');
       }
-
     });
-
   });
 
-  describe("#hasFileChanged", () => {
+  describe('#hasFileChanged', () => {
+    describe('when file already exists in S3', () => {
+      describe('and last modified dates do not match', () => {
+        it('should resolve to false', async () => {
+          const s3 = rewire('../src/s3');
+          const hasFileChanged = s3.__get__('hasFileChanged');
+          const res = {
+            Metadata: {
+              'last-modified': '2017-09-12T15:48:06.228Z',
+            },
+          };
 
-    it('Should resolve to false when the dates match', async () => {
-      const s3 = rewire('../src/s3');
-      const hasFileChanged = s3.__get__('hasFileChanged');
-      const res = {
-        "Metadata": {
-          "last-modified": "2017-09-12T15:48:06.228Z"
+          const s3ClientMock = {
+            headObject(params, cb) {
+              cb(null, res);
+            },
+          };
+
+          function fileModifiedCheckFn() {
+            return new Promise((resolve) => {
+              resolve(new Date('2017-09-12T15:48:06.228Z'));
+            });
           }
-      };
 
-      const s3ClientMock = {
-        headObject: function (params, cb) {
-          cb(null, res);
-        }
-      };
+          const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFn);
 
-      const fileModifiedCheckFn = function () {
-        return new Promise((resolve, reject) => {
-          resolve(new Date("2017-09-12T15:48:06.228Z"));
+          actual.should.equal(false);
         });
-      };
+      });
 
-      const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFn);
+      describe('and last modified dates match', () => {
+        const s3 = rewire('../src/s3');
+        const hasFileChanged = s3.__get__('hasFileChanged');
+        const res = {
+          Metadata: {
+            'last-modified': '2017-09-12T15:48:06.228Z',
+          },
+        };
 
-      actual.should.equal(false);
+        const s3ClientMock = {
+          headObject(params, cb) {
+            cb(null, res);
+          },
+        };
+
+        function fileModifiedCheckFn() {
+          return new Promise((resolve) => {
+            resolve(new Date('2007-09-12T15:48:06.228Z'));
+          });
+        }
+
+        const fileModifiedCheckFnSpy = sinon.spy(fileModifiedCheckFn);
+
+        it('`hasFileChanged(..)` should be called with the correct arguments', async () => {
+          await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFnSpy);
+          // eslint-disable-next-line no-unused-expressions
+          fileModifiedCheckFnSpy.calledWith('public/foo.txt').should.be.ok;
+        });
+
+        it('should resolve to true', async () => {
+          const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFnSpy);
+          actual.should.equal(true);
+        });
+      });
+
+      describe('and last modified date is not available for remote file', () => {
+        it('should resolve to true (in order to instigate an upload', async () => {
+          const s3 = rewire('../src/s3');
+          const hasFileChanged = s3.__get__('hasFileChanged');
+          const res = {
+            Metadata: {},
+          };
+
+          const s3ClientMock = {
+            headObject(params, cb) {
+              cb(null, res);
+            },
+          };
+
+          function fileModifiedCheckFn() {
+            return new Promise((resolve) => {
+              resolve(new Date('2017-09-12T15:48:06.228Z'));
+            });
+          }
+
+          const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFn);
+
+          actual.should.equal(true);
+        });
+      });
     });
 
-    it('Should resolve to true when the dates match', async () => {
-      const s3 = rewire('../src/s3');
-      const hasFileChanged = s3.__get__('hasFileChanged');
-      const res = {
-        "Metadata": {
-          "last-modified": "2017-09-12T15:48:06.228Z"
+    describe('when file does not already exist in S3', () => {
+      it('should resolve to true (in order to instigate an upload)', async () => {
+        const s3 = rewire('../src/s3');
+        const hasFileChanged = s3.__get__('hasFileChanged');
+        const errorRes = {
+          code: 'NotFound',
+        };
+
+        const s3ClientMock = {
+          headObject(params, cb) {
+            cb(errorRes, {});
+          },
+        };
+
+        function fileModifiedCheckFn() {
+          return new Promise((resolve) => {
+            resolve(new Date('2017-09-12T15:48:06.228Z'));
+          });
         }
-      };
 
-      const s3ClientMock = {
-        headObject: function (params, cb) {
-          cb(null, res);
+        const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFn);
+        actual.should.equal(true);
+      });
+    });
+
+    describe('when a error is encountered getting the object from S3', () => {
+      it('should reject with the error', async () => {
+        const s3 = rewire('../src/s3');
+        const hasFileChanged = s3.__get__('hasFileChanged');
+        const errorRes = {
+          code: 'AnyErrorButNotFound',
+        };
+
+        const s3ClientMock = {
+          headObject(params, cb) {
+            cb(errorRes, {});
+          },
+        };
+
+        function fileModifiedCheckFn() {
+          return new Promise((resolve) => {
+            resolve(new Date('2017-09-12T15:48:06.228Z'));
+          });
         }
-      };
 
-      const fileModifiedCheckFn = function () {
-        return new Promise((resolve, reject) => {
-          resolve(new Date("2007-09-12T15:48:06.228Z"));
-        });
-      };
-
-      const spy = sinon.spy(fileModifiedCheckFn);
-
-      const actual = await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, spy);
-
-      spy.calledWith('public/foo.txt').should.be.ok;
-      actual.should.equal(true);
+        try {
+          await hasFileChanged('foo.txt', 'myBucket', s3ClientMock, fileModifiedCheckFn);
+          'I'.should.equal('Me');
+        } catch (e) {
+          e.should.equal(errorRes);
+        }
+      });
     });
   });
 });
